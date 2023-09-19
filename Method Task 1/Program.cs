cf957364718d2ca97dd44640a5a421bda002cf6a
e.georgieva24 <e.georgieva24@LAB108PC13>
2023-09-19 15:23:35 +0300

    Add project files.

---
+++ Method Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        WelcomeFriends();
+    }
+    static void WelcomeFriends()
+    {
+        Console.WriteLine("Welcome Friends!");
+        Console.WriteLine("Have a nice day!");
+    }
+}
